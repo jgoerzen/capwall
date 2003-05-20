050338b9624254cc7e4c322fb7ddf1f059bcd923
jgoerzen <jpgarch@complete.org>
2003-05-20 16:28:39 -0500

    /capwall/head: changeset 109
    Now it's working.
    
    darcs-hash:20030520212839-a5eff-d14c096f1bb34c6a9c2ac4b1ed18e36ab813413c.gz

--- capwall.c
+++ capwall.c
@@ -10,9 +10,17 @@
 #include <time.h>
 #include <string.h>
 #include <sys/select.h>
-
+#include <stdio.h>
 
 #define _SC(a) if ((a) == -1) {perror(NULL); exit(202);}
+struct utmp utentry;
+int loggedin = 0;
+
+void cleanup(void) {
+  if (loggedin) {
+    logout(utentry.ut_line);
+  }
+}
 
 void getmessage(int fd, char *buffer, const int bufsize, int *count) {
   time_t starttime;
@@ -22,7 +30,7 @@ void getmessage(int fd, char *buffer, const int bufsize, int *count) {
 
   sizeavail = bufsize - 3;
   *count = 0;
-  *count = read(fd, buffer, 1);
+  _SC(*count = read(fd, buffer, 1));
 
   if (*count < 1) {
     exit(255);
@@ -41,7 +49,7 @@ void getmessage(int fd, char *buffer, const int bufsize, int *count) {
       timeout.tv_sec = 1;
     }
     if (select(fd + 1, &rfds, NULL, NULL, &timeout)) {
-      tmpint = read(fd, buffer + (*count), sizeavail);
+      _SC(tmpint = read(fd, buffer + (*count), sizeavail));
       if (tmpint < 1) {
 	exit(255);
       }
@@ -57,27 +65,33 @@ int main(int argc, char *argv[]) {
   int masterfd, slavefd;
   char ptyname[1024];
   char buffer[10240];
-  struct utmp utentry;
   int count;
+  FILE *dest;
 
+  if (! (argc > 1)) {
+    printf("Syntax: capwall command\n");
+    exit(1);
+  }
+  
   _SC(openpty(&masterfd, &slavefd, ptyname, NULL, NULL));
-  printf("Allocated PTY at %s\n", ptyname);
   _SC(dup2(slavefd, 0));
-  printf("Done dup2\n");
   strncpy(utentry.ut_user, getpwuid(getuid())->pw_name, UT_NAMESIZE);
   utentry.ut_user[UT_NAMESIZE-1] = 0;
   strncpy(utentry.ut_host, "N/A", UT_HOSTSIZE);
   utentry.ut_host[UT_HOSTSIZE-1] = 0;
   gettimeofday(&(utentry.ut_tv), NULL);
   login(&utentry);
-  printf("Done login\n");
-  
+  loggedin = 1;
+  atexit(cleanup);
+
   while (1) {
     getmessage(masterfd, buffer, sizeof(buffer), &count);
-    printf("----- START OF MESSAGE -----\n");
-    fflush(stdout);
-    write(1, buffer, count);
-    printf("----- END OF MESSAGE -----\n");
+    dest = popen(argv[1], "w");
+    if (!dest) {
+      continue;
+    }
+    fwrite(buffer, count, 1,  dest);
+    pclose(dest);
 
   }
   return 0;
